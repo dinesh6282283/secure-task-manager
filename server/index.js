ec84dff286de12f31cce6a44bce125d52d64495e
dinesh <dinesh6282283@gmail.com>
2026-01-12 19:37:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://secure-task-manager.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://secure-task-manager-1.onrender.com/api",
+    ],
     credentials: true,
   })
 );
